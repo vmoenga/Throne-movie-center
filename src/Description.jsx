e8cae91b509362c9edb5748a07c208c46983979f
vmoenga <vmoenga36@gmail.com>
2024-05-12 11:24:37 +0300

    new add

--- src/Description.jsx
+++ src/Description.jsx
@@ -5,11 +5,11 @@ import React from 'react';
 const Description = () => {
   return (
     <div className="description">
-      <p>Welcome to Throne, where every frame tells a story. 
-        Explore our vast collection of movie posters spanning genres, decades, and cultures. 
-        Immerse yourself in the captivating visuals that have defined cinematic history. 
-        From timeless classics to the latest blockbusters, Throne is your gateway to the world of film artistry. 
-        Find your next favorite poster and let the magic of cinema adorn your walls..</p>
+      <div className="description-block">
+        <h2>Welcome to Throne Movie Center</h2>
+        <p>Embark on a cinematic journey like never before! At Throne Movie Center, every frame tells a story, and every poster is a masterpiece waiting to adorn your walls.
+           Explore our meticulously curated collection of movie posters, spanning genres, decades, and cultures. Immerse yourself in the captivating visuals that have defined cinematic history.</p>
+      </div>
     </div>
   );
 };
